fa1e5c86fb40f64ee959d34277d648be0c1cb3dd
Jan Krajewski <jan.krajewski.it@gmail.com>
2023-12-22 15:40:15 +0100

    Additional linters + format on save

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,7 +1,10 @@
-import type { AppProps } from 'next/app'
-import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
+import type { AppProps } from "next/app";
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
 
- 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <AppCacheProvider><Component {...pageProps} /></AppCacheProvider> 
+  return (
+    <AppCacheProvider>
+      <Component {...pageProps} />
+    </AppCacheProvider>
+  );
 }
--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,18 +1,24 @@
-import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
-import { DocumentHeadTags, documentGetInitialProps, DocumentHeadTagsProps } from '@mui/material-nextjs/v14-pagesRouter';
+import { Html, Head, Main, NextScript, DocumentProps } from "next/document";
+import {
+  DocumentHeadTags,
+  documentGetInitialProps,
+  DocumentHeadTagsProps,
+} from "@mui/material-nextjs/v14-pagesRouter";
 
-export default function MyDocument(props: DocumentProps & DocumentHeadTagsProps) {
+export default function MyDocument(
+  props: DocumentProps & DocumentHeadTagsProps,
+) {
   return (
     <Html lang="en">
-      <Head >
-        <DocumentHeadTags {...props} /> 
+      <Head>
+        <DocumentHeadTags {...props} />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
 
 MyDocument.getInitialProps = documentGetInitialProps;
